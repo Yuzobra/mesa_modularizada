c57eed0d5551bf21257b006d3e5b5c3fcd47c129
Thor99 <thor.fc21@gmail.com>
2016-03-09 11:47:48 -0300

    initial commit

---
+++ mesa.ino
@@ -0,0 +1,41 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11);
+String command;
+int led1 = 13;
+int led2 = 12;
+
+void setup()
+{
+
+  Serial.begin(9600);
+  Serial.println("Digite os comandos AT :");
+
+  mySerial.begin(9600);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+}
+
+void loop()
+{
+  if (mySerial.available())
+  {
+     while(mySerial.available())
+     {
+       delay(1);
+       command += (char)mySerial.read();
+     }
+   Serial.println(command);
+
+   if(command == "*Ligar LED#"){
+     digitalWrite(led1, HIGH);
+     digitalWrite(led2, HIGH);
+   }
+
+   if(command == "*apagar#"){
+     digitalWrite(led1, LOW);
+     digitalWrite(led2, LOW);
+   }
+   command = "";
+  }
+}
